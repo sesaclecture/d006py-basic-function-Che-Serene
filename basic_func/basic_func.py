74afd66e5a07cb494b4dd9377fe2476711867919
Serene <frostn@naver.com>
2025-09-02 11:18:01 +0900

    제출,,,,,,,,,,,,,

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,33 @@
 def add(a, b):
-    pass
+    return a+b
 
 
 def sub(a, b):
-    pass
+    return a-b
 
 
 def mul(a, b):
-    pass
+    return a*b
 
 
 def div(a, b):
-    pass
+    return a/b
 
 
 def power(base, pow):
-    pass
+    return base**pow
 
 
 def square(base):
-    pass
+    return base**2
 
 
 def greet(이름="낯선자", 나이=20):
-    pass
+    if 나이 >= 50 :
+        greet_message = "안녕하십니까"
+    elif 나이 < 20 :
+        greet_message = "안녕"
+    else :
+        greet_message = "안녕하신가"
+
+    return f"{greet_message} {이름}!"
